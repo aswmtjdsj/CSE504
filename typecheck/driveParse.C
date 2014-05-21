2202e6182083c461bf851cf7e3c37d65948eb895
Parallels <yunpeng.shaw@gmail.com>
2014-05-21 15:05:03 -0400

    1

--- typecheck/driveParse.C
+++ typecheck/driveParse.C
@@ -220,10 +220,10 @@ main(int argc, char *argv[], char *envp[]) {
   labelNum = 0;
   if (ge != NULL) {
 	//cout << "Finished parsing, here is the AST\n";
-	//ge->print(cout, 0);
-  ge->typeCheck();
+	ge->print(cout, 0);
+  //ge->typeCheck();
   cout << endl;
-  ge->typePrint(cout, 0);
+  //ge->typePrint(cout, 0);
   //EFSAlist* codeList = ge->codeGen();
   //codeList->codePrint(cout);
   }
